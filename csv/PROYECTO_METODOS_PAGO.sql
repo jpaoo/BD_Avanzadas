d6d4119a6972b63e3568a7394633d8a49d121917
jpaoo <A01208471@itesm.mx>
2018-02-14 15:48:25 -0600

    fix data

--- csv/PROYECTO_METODOS_PAGO.sql
+++ csv/PROYECTO_METODOS_PAGO.sql
@@ -1,3 +1,4 @@
 insert into PROYECTO_METODOS_PAGO (ID_METODO_PAGO, NOMBRE) values (1, 'Efectivo');
 insert into PROYECTO_METODOS_PAGO (ID_METODO_PAGO, NOMBRE) values (2, 'Crédito');
 insert into PROYECTO_METODOS_PAGO (ID_METODO_PAGO, NOMBRE) values (3, 'Débito');
+insert into PROYECTO_METODOS_PAGO (ID_METODO_PAGO, NOMBRE) values (4, 'Meses sin intereses');
