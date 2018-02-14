bb4f88602c5969198767ded02ae161300e3a04a1
jpaoo <A01208471@itesm.mx>
2018-02-14 14:44:30 -0600

    data

---
+++ csv/PROYECTO_SUCURSALES.sql
@@ -0,0 +1,4 @@
+insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION) values (1, '2648 Blue Bill Park Circle');
+insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION) values (2, '885 Fairview Trail');
+insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION) values (3, '0 Pankratz Trail');
+insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION) values (4, '212 Hollow Ridge Pass');
