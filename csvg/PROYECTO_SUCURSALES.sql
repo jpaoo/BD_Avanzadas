4c742b18451f16ab08561546939871f88ed7f3c6
Kary <karyrs15@gmail.com>
2018-03-22 13:45:32 -0600

    Cambiar insert a sucursales

--- csvg/PROYECTO_SUCURSALES.sql
+++ csvg/PROYECTO_SUCURSALES.sql
@@ -1,4 +1,4 @@
-insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION) values (1, '2648 Blue Bill Park Circle', 'JP');
-insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION) values (2, '885 Fairview Trail', 'MM');
-insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION) values (3, '0 Pankratz Trail', 'KR');
-insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION) values (4, '212 Hollow Ridge Pass', 'MC');
+insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION, NOMBRE) values (1, '2648 Blue Bill Park Circle', 'JP');
+insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION, NOMBRE) values (2, '885 Fairview Trail', 'MM');
+insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION, NOMBRE) values (3, '0 Pankratz Trail', 'KR');
+insert into PROYECTO_SUCURSALES (ID_SUCURSAL, DIRECCION, NOMBRE) values (4, '212 Hollow Ridge Pass', 'MC');
